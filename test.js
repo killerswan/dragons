0786f57d287b1d4a0c27cb2c1a46b04b80c0a5bb
Kevin Cantu <me@kevincantu.org>
2011-01-15 20:22:01 -0800

    now we are building an SVG path!

--- test.js
+++ test.js
@@ -69,3 +69,114 @@ var ss = [mm, nn];
 show(growSet(ss))
 show(growSet(growSet(ss)))
 show(growSet(growSet(growSet(ss))))
+
+
+var toSVG = function (point0, point1) { 
+   return "M" + point0[0] + " " + point0[1] + "L" + point1[0] + point1[1];
+}
+
+/*
+// given html with a notepad element id
+var paper = Raphael("notepad", 320, 200);
+// todo: also make growSetN(n, growset) -> growSet^n
+var c = paper.path(toSVG( growSet(growSet(growSet(ss))) ));
+*/
+
+show([].concat([1,2,3],[4],[5,[6]], 7));
+
+var insertF = function (list, func) {
+   // note, this is destructive to argument `list`
+
+   var helper = function (a, b, cs) {
+      // note: concat takes arrays and combines them, 
+      //       so to combine arrays of arrays we need to box things a bit
+      //       hence [a] and [b]
+      if (cs.length > 0) {
+         //a : k (a,b) : h(b, head cs, tail cs)
+         // don't box the recursive call
+         return [].concat([a], [func(a,b)], helper( b, cs.shift(), cs));
+      
+      } else {
+         //a : k (a,b) : b
+         return [].concat([a], [func(a,b)], [b]);
+      }
+   }
+
+   if (list.length >= 2) {
+      return helper( list.shift(), list.shift(), list );
+   } else {
+      return [0];
+   }
+}
+
+var mm = [3, 1];
+var nn = [5, 10];
+var oo = [7, 15];
+tt = [mm, nn, oo];
+uu = [mm, nn];
+
+var newstuff = insertF( uu, function (a, c) {
+   return [0];
+});
+var newstuffprime = insertF( tt, function (a, c) {
+   return [0];
+});
+
+
+show(newstuff);
+show(newstuffprime);
+
+tt = [mm, nn, oo];
+
+var better = insertF( tt, function (a, c) {
+   // define and close over side if needed, but oscillate with each call
+   var side = side || 0;
+   side = !side;
+   return M.plus(a, 
+                 M.mult( side ? left : right, M.minus(c, a) ));
+});
+
+show(better);
+
+var between = function (list, func) {
+   // note, this is destructive to argument `list`
+
+   var helper = function (a, b, cs) {
+      // note: concat takes arrays and combines them, 
+      //       so to combine arrays of arrays we need to box things a bit
+      if (cs.length > 0) {
+         //a : k (a,b) : h(b, head cs, tail cs)
+         // don't box the recursive call
+         return [].concat([func(a,b)], helper( b, cs.shift(), cs));
+      
+      } else {
+         //a : k (a,b) : b
+         return [].concat([func(a,b)]);
+      }
+   }
+
+   if (list.length >= 2) {
+      return helper( list.shift(), list.shift(), list );
+   } else {
+      return [7]; // srsly, throw error instead
+   }
+}
+
+
+tt = [mm, nn, oo];
+
+show(between(tt, toSVG));
+
+// set of points -> set of paths -> one path
+var toPath = function (set) {
+   return between(set, toSVG).join();
+}
+
+tt = [mm, nn, oo];
+show(toPath(tt));
+
+tt = [mm, nn];
+show(toPath(tt));
+
+
+
